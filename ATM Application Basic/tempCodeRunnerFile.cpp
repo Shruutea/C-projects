544c38a307f120396117f21cd3cffc60d7df8d99
Shruutea <94055233+Shruutea@users.noreply.github.com>
2023-02-19 18:25:53 +0530

    Add files via upload
    Basic ATM application with deposit, withdraw, and check balance options on cpp

---
+++ ATM Application Basic/tempCodeRunnerFile.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+// void showMenu(){
+//     cout<<"*****MENU*****"<<endl;
+//     cout<<"1. Check balance" <<endl;
+//     cout<<"2. Deposit" <<endl;
+//     cout<<"3. Withdraw" <<endl;
+//     cout <<"4. Exit" << endl;
+//     cout<< "**********" <<endl;
+// }
+
+int main()
+{
+    //Show menu, Check Balance , Deposit , Withdrawl
+    int option;
+    double balance =  500;
+
+    do
+    {
+        showMenu();
+        cout << "Option: ";
+        cin >> option;
+
+        switch (option)
+        {
+        case 1:
+            cout << "Balance is: " << balance << "Rs" << endl;
+            break;
+
+        case 2:
+            cout << "Deposit Amount: ";
+            double depositAmount;
+            cin >> depositAmount;
+            balance += depositAmount;
+            break;
+
+        case 3:
+            cout << "Withdraw Amount: ";
+            double withdrawAmount;
+            cin >> withdrawAmount;
+            if (withdrawAmount <= balance)
+                balance -= withdrawAmount;
+            else
+                cout << "not enough money" << endl;
+            break;
+        }
+    } 
+    
+    while (option != 4)
+    
+
+    
+    // return 0;
+}
